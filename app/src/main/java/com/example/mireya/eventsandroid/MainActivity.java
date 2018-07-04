7dc14d743f1c226b2b24c40ac8a8a95f547ef52a
Mireya27 <anamireyaj@gmail.com>
2018-07-04 10:55:08 -0500

    intalando fabric

--- app/src/main/java/com/example/mireya/eventsandroid/MainActivity.java
+++ app/src/main/java/com/example/mireya/eventsandroid/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.mireya.eventsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        //btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
